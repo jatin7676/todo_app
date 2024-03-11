308e066ba11f20028519c7349c30a19436b996ed
jatin yadav <130964755+jatin7676@users.noreply.github.com>
2024-03-11 14:29:22 +0530

    Add files via upload

---
+++ execute1.bat
@@ -0,0 +1,7 @@
+echo "# todo_app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jatin7676/todo_app.git
+git push -u origin main
